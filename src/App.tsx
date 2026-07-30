f97d91e8c0828684cb8022062e274b5f0193306f
orihina0193 <alexandrite0193@hotmail.co.jp>
2026-07-30 12:49:35 +0900

    Update src/App.tsx via SoundBox Cloud Sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=1f31155a"; const Fragment = __vite__cjsImport0_react_jsxDevRuntime["Fragment"]; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
-import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=1f31155a"; const useState = __vite__cjsImport1_react["useState"]; const useEffect = __vite__cjsImport1_react["useEffect"];
-import { Disc, Database, HelpCircle, Github } from "/node_modules/.vite/deps/lucide-react.js?v=1f31155a";
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=17b4195c"; const Fragment = __vite__cjsImport0_react_jsxDevRuntime["Fragment"]; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=17b4195c"; const useState = __vite__cjsImport1_react["useState"]; const useEffect = __vite__cjsImport1_react["useEffect"];
+import { Disc, Database, HelpCircle, Github } from "/node_modules/.vite/deps/lucide-react.js?v=17b4195c";
 import { AppMode } from "/src/types.ts";
 import { getTracks } from "/src/lib/db.ts";
 import Extractor from "/src/components/Extractor.tsx";
